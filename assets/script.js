ec1c6eb3d19c294a1473d86f1e0c5606274d1940
Huseyin-Kaplan <kaplanhuseyin1999@gmail.com>
2023-03-10 02:48:11 +0300

    some ui fix

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 const newDeckBtn = document.getElementById("newDeck")
 const newCard = document.getElementById("newCard")
 let deckID
+let computerScore = 0
+let yourScore = 0
 const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
      "10", "JACK", "QUEEN", "KING", "ACE"]
 const newDeckFunc = () => {
@@ -31,7 +33,7 @@ const newCardFunc = () => {
                     values(data.cards[0].value, data.cards[1].value, data.cards[2].value,data.cards[3].value , data.cards[4].value, data.cards[5].value)
                   
 
-               // const cameData = values(data.cards[0].value, data.cards[1].value, data.cards[2].value, data.cards[3].value, data.cards[4].value, data.cards[5].value  )
+               
 
 
           })
@@ -47,15 +49,14 @@ const values = (cards0, cards1, cards2, cards3, cards4 , cards5) => {
      const playerThreeValue = valueOptions.indexOf(cards2)
 
      let playerOneScores = [playerOneValue, playerTwoValue, playerThreeValue]
-     
+
   
      for( let i = 0; i<=2; i++){
           document.querySelector(`.value${i}`).textContent = playerOneScores[i]+2
-         
      }
 
-     let sum = (playerOneValue+2) + (playerTwoValue+2) + (playerThreeValue+2)
-     document.querySelector(".score-sign").textContent = sum
+     let yourSum = (playerOneValue+2) + (playerTwoValue+2) + (playerThreeValue+2)
+     document.querySelector(".score-sign").textContent = yourSum
 
      const playerFourValue = valueOptions.indexOf(cards3)
      const playerFiveValue = valueOptions.indexOf(cards4)
@@ -65,10 +66,26 @@ const values = (cards0, cards1, cards2, cards3, cards4 , cards5) => {
      for( let i = 3; i<=5; i++){
           document.querySelector(`.value${i}`).textContent = scores[i]+2
      }
-     let sum2 = (playerFourValue+2) + (playerFiveValue+2) + (playerSixValue+2)
-     document.querySelector(".score-sign2").textContent = sum2
+     let pcSum = (playerFourValue+2) + (playerFiveValue+2) + (playerSixValue+2)
+     document.querySelector(".score-sign2").textContent = pcSum
 
-     return sum2    
+     if(yourSum > pcSum){
+          yourScore++
+          document.getElementById("scoreOverAllYour").textContent = yourScore
+          if(yourScore>=10){
+               document.getElementById("scoreOverAllYour").style.color = "red" 
+          }
+
+     }
+
+     else if (pcSum > yourSum){
+          computerScore++
+          document.getElementById("scoreOverAllPc").textContent = computerScore
+          if(computerScore>=10){
+               document.getElementById("scoreOverAllPc").style.color = "red" 
+          }
+     }
+     
 }
 
 
